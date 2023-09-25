efc0120339ae2501a5d7576673e0faa71349b652
Gert Glükmann <gglukmann@gmail.com>
2023-09-25 15:46:03 +0300

    Change port

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.all(
       }),
 );
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, async () => {
   console.log(`Express server listening on port ${port}`);
 
