a187a0514feccf5395d2656fd9ca9a76ec62c849
Gert Glükmann <gglukmann@gmail.com>
2026-05-28 11:09:25 +0300

    feat: Refactor static asset serving in production server to dynamically handle multiple directories, improving code maintainability and organization.

--- server.js
+++ server.js
@@ -34,12 +34,14 @@ if (DEVELOPMENT) {
   });
 } else {
   console.log('Starting production server');
-  app.use(
-    '/assets',
-    express.static('build/client/assets', { immutable: true, maxAge: '1y' }),
-  );
-  app.use(morgan('tiny'));
+  for (const dir of ['assets', 'fonts', 'favicons']) {
+    app.use(
+      `/${dir}`,
+      express.static(`build/client/${dir}`, { immutable: true, maxAge: '1y' }),
+    );
+  }
   app.use(express.static('build/client', { maxAge: '1h' }));
+  app.use(morgan('tiny'));
   app.use(await import(BUILD_PATH).then((mod) => mod.app));
   app.use(
     helmet({
